05dedb6bce1db3b1de4646046f0d328844dc2935
Hleb Kanonik <50049166+Cyberglamdring@users.noreply.github.com>
2019-09-05 14:52:58 +0300

    Update docker-jenkins-install.sh

--- Jenkins/docker-jenkins-install.sh
+++ Jenkins/docker-jenkins-install.sh
@@ -1,5 +1,6 @@
 # first of all Dicker -> settings -> Shared Drivers -> select
 # on drive create folder jenkins_home
-$containerId=967eb2e3dc6b
-docker run -d -p 8080:8080 -p 50000:50000 -v c:/jenkins_home:/var/jenkins_home jenkins
+sharedDrive=c:/jenkins_home
+containerId=967eb2e3dc6b
+docker run -d -p 8080:8080 -p 50000:50000 -v $sharedDrive:/var/jenkins_home jenkins
 docker exec -it $containerId cat /var/jenkins_home/secrets/initialAdminPassword
