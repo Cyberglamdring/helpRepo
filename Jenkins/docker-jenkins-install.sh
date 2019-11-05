fa66012c312be32e6dbba5c5fd6f8cb4d6d44bf8
Hleb Kanonik <esscyh@gmail.com>
2019-11-05 15:56:17 +0300

    maven project

--- Jenkins/docker-jenkins-install.sh
+++
@@ -1,6 +0,0 @@
-# first of all Dicker -> settings -> Shared Drivers -> select
-# on drive create folder jenkins_home
-sharedDrive=c:/jenkins_home
-containerId=967eb2e3dc6b
-docker run -d -p 8080:8080 -p 50000:50000 -v $sharedDrive:/var/jenkins_home jenkins
-docker exec -it $containerId cat /var/jenkins_home/secrets/initialAdminPassword
